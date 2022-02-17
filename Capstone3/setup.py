77397277517e151d0f9b1f31931ead697fcd1c3c
Snizzmo <Cfranznick@gmail.com>
2022-02-17 14:43:41 -0600

    begin capstone3

---
+++ Capstone3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is my 3rd capstone project for springboard, where I take book information and reviews and make a recommendation system.',
+    author='Charles Franznick',
+    license='MIT',
+)
