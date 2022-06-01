76020cf865fb6e2679fc30e04738db8a1bdc32ce
Snizzmo <Cfranznick@gmail.com>
2022-06-01 15:55:08 -0500

    init

---
+++ WALS/docs/index.rst
@@ -0,0 +1,24 @@
+.. WALS documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+WALS documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ WALS/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a data science project focusing on languages and their features, as described by The World Atlas of Language Structures (WALS). WALS is a large database of structural (phonological, grammatical, lexical) properties of languages gathered from descriptive materials (such as reference grammars) by a team of 55 authors. ',
+    author='Charles Franznick',
+    license='MIT',
+)
